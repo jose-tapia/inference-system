82096ac696fc4a0ce106db45f9c3995da22299b5
jose-tapia <jose.tapiaav@gmail.com>
2021-11-11 12:50:38 -0600

    Convert a nested clause into a Clause / CNF form

--- CNF.py
+++ CNF.py
@@ -1,5 +1,5 @@
 from os import error
-from Clause import Literal, Clause, NestedClause, Operations
+from Clause import Literal, Clause, NestedClause, Operations, ClauseType
 
 def simplify_clause(clause):
     if isinstance(clause, NestedClause):
@@ -16,7 +16,7 @@ def simplify_clause(clause):
         elif clause.operation == Operations.Implication:
             clause.left_clause = simplify_clause(clause.left_clause).complement()
             clause.right_clause = simplify_clause(clause.right_clause)
-            clause.operation = Operations.Conjuntion
+            clause.operation = Operations.Disjuntion
             return clause
 
         elif clause.operation == Operations.LeftImplication:
@@ -36,15 +36,115 @@ def simplify_clause(clause):
         return clause
 
 
-def distributive_conjuntion(clause):
-    pass
+def distributive_conjuntion(left_clause, right_clause):
+    if isinstance(left_clause, Clause) and left_clause.type == ClauseType.F:
+        return Clause(type=ClauseType.F)
+    if isinstance(left_clause, Clause) and left_clause.type == ClauseType.T:
+        return right_clause
+    if isinstance(right_clause, Clause) and right_clause.type == ClauseType.F:
+        return Clause(type=ClauseType.F)
+    if isinstance(right_clause, Clause) and right_clause.type == ClauseType.T:
+        return left_clause
+    
+    if isinstance(left_clause, Literal):
+        if isinstance(right_clause, Literal):
+            return Clause([left_clause, right_clause], Operations.Conjuntion)
+
+        elif right_clause.operation == Operations.Conjuntion:
+            return Clause([left_clause]+right_clause.clauses, Operations.Conjuntion)
+
+        else:
+            clauses_dist = [distributive_conjuntion(left_clause, clause).simplify() for clause in right_clause.clauses]
+            return Clause(clauses_dist, Operations.Disjuntion)
+
+    if isinstance(right_clause, Literal):
+        return distributive_conjuntion(right_clause, left_clause)
+
+    if left_clause.operation == Operations.Conjuntion:
+        if right_clause.operation == Operations.Conjuntion:
+            clauses_dist = left_clause.clauses + right_clause.clauses
+            return Clause(clauses_dist, Operations.Conjuntion)
+        
+        else:
+            clauses_dist = [distributive_conjuntion(left_clause, clause).simplify() for clause in right_clause.clauses]
+            return Clause(clauses_dist, Operations.Disjuntion)
+        
+    else:
+        clauses_dist = [distributive_conjuntion(clause, right_clause).simplify() for clause in left_clause.clauses]
+        return Clause(clauses_dist, Operations.Disjuntion) 
+
+def union_disjuntion(left_clause, right_clause):
+    if isinstance(left_clause, Clause) and left_clause.type == ClauseType.T:
+        return Clause(type=ClauseType.T)
+    if isinstance(left_clause, Clause) and left_clause.type == ClauseType.F:
+        return right_clause
+    
+    if isinstance(right_clause, Clause) and right_clause.type == ClauseType.T:
+        return Clause(type=ClauseType.T)
+    if isinstance(right_clause, Clause) and right_clause.type == ClauseType.F:
+        return left_clause
+    
+    if isinstance(left_clause, Literal):
+        if isinstance(right_clause, Literal):
+            return Clause([left_clause, right_clause], Operations.Disjuntion)
+        
+        elif right_clause.operation == Operations.Disjuntion:
+            return Clause([left_clause] + right_clause.clauses, Operations.Disjuntion)
+        
+        else:
+            return Clause([left_clause, right_clause], Operations.Disjuntion)
+    
+    if isinstance(right_clause, Literal):
+        return union_disjuntion(right_clause, left_clause)
+    
+    if left_clause.operation == Operations.Conjuntion:
+        if right_clause.operation == Operations.Conjuntion:
+            return Clause([left_clause, right_clause], Operations.Disjuntion)
+
+        else:
+            return Clause([left_clause] + right_clause.clauses, Operations.Disjuntion)
+    
+    else:
+        if right_clause.operation == Operations.Conjuntion:
+            return Clause(left_clause.clauses + [right_clause], Operations.Disjuntion)
+        
+        else:
+            clauses_dist = left_clause.clauses + right_clause.clauses
+            return Clause(clauses_dist, Operations.Disjuntion)                    
 
 def flat_clause(clause):
-    pass 
-
+    if isinstance(clause, NestedClause):
+        left_clause = flat_clause(clause.left_clause)
+        right_clause = flat_clause(clause.right_clause)
+        if clause.operation == Operations.Disjuntion:
+            result = union_disjuntion(left_clause, right_clause).simplify()
+        elif clause.operation == Operations.Conjuntion:
+            result = distributive_conjuntion(left_clause, right_clause).simplify()
+        return result
+    else:
+        return clause
+        
 def CNF(clause):
     """
     Given a nested clause, transform it to a CNF
     """
     clause_simplified = simplify_clause(clause) 
     return flat_clause(clause_simplified)   
+
+if __name__ == '__main__':
+    
+    a = Literal('a')
+    b = Literal('b')
+    na = Literal('a', True)
+    A = NestedClause(a, b, Operations.Disjuntion)
+    B = NestedClause(a, b, Operations.Conjuntion)
+    C = NestedClause(a, b, Operations.Implication)
+    D = NestedClause(a, b, Operations.Equivalent)
+    
+    #print(str(A), str(B), str(C), str(D), sep='\n')
+    
+    E = CNF(B)
+    F = CNF(C)
+    G = CNF(D)
+    #print(str(G))
+    print(str(E), str(F), str(G), sep='\n')
--- Clause.py
+++ Clause.py
@@ -9,6 +9,10 @@ class Operations(Enum):
     LeftImplication = '<='
     Equivalent = '<=>'
 
+class ClauseType(Enum):
+    Clause = ''
+    T = 'T'
+    F = 'F'
 class Literal:
     # Literal class
     def __init__(self, literal:str='', neg:bool=False):
@@ -32,7 +36,7 @@ class Literal:
         return Literal(self.literal, not self.neg)
 
 class Clause:
-    def __init__(self, clauses=[], operation=Operations.Disjuntion):
+    def __init__(self, clauses=[], operation=Operations.Disjuntion, type=ClauseType.Clause):
         """
         Wrap a disjuntion or conjuntion of a list of clauses in CNF form
         
@@ -41,14 +45,21 @@ class Clause:
             - operation: Indicates which operation is apply between clauses
         """
         self.clauses = clauses
-        self.operation = operation
-    
+        if operation == Operations.Disjuntion or operation == Operations.Conjuntion:
+            self.operation = operation
+        else:
+            raise error(f'Operation not supported yet "{self.operation.name}"')
+        self.type = type
+
     def __str__(self):
+        if self.type == ClauseType.T or self.type == ClauseType.F:
+            return self.type.value
+        
         if len(self.clauses) == 0:
             return ''
         if len(self.clauses) == 1:
             return str(self.clauses[0])
-        clauses_str = [f'({str(clause)})' for clause in self.clauses]
+        clauses_str = [str(clause) if isinstance(clause, Literal) else f'({str(clause)})' for clause in self.clauses]
         return f' {self.operation.value} '.join(clauses_str)
     
     def __eq__(self, clause):
@@ -58,25 +69,66 @@ class Clause:
         # No supported other comparisons for now
         if not isinstance(clause, Clause):
             return False
+        
+        if self.type != clause.type:
+            return False
+        if self.type != ClauseType.Clause:
+            return True
+        
         # If length of clauses are not equal, then the clauses are different
         if len(self.clauses) != len(clause.clauses):
             return False
+        
+        if len(self.clauses) > 1 and self.operation != clause.operation:
+            return False
+        
         # Compare Literals
         if len(self.clauses) == 1:
             return self.clauses[0] == clause.clauses[0]
         # Check that has the same elements
-        for clause in self.clauses:
-            if clause not in clause.clauses:
+        for clause_int in self.clauses:
+            if clause_int not in clause.clauses:
                 return False
         return True
     
     def complement(self):
         if self.operation == Operations.Disjuntion:
-            self.operation = Operations.Conjuntion
+            operation_comp = Operations.Conjuntion
         else:
-            self.operation = Operations.Disjuntion
+            operation_comp = Operations.Disjuntion
 
-        self.clauses = [clause.complement() for clause in self.clauses]
+        clauses_comp = [clause.complement() for clause in self.clauses]
+        return Clause(clauses_comp, operation_comp)
+        
+    def simplify(self):
+        if self.type == ClauseType.T or self.type == ClauseType.F:
+            return Clause(type=self.type)
+        
+        clauses_unique = []
+        exists_complement = False
+        exists_T = False
+        exists_F = False
+        for clause in self.clauses:
+            if isinstance(clause, Clause) and clause.type != ClauseType.Clause:
+                if clause.type == ClauseType.T:
+                    exists_T = True
+                elif clause.type == ClauseType.F:
+                    exists_F = True                
+            elif clause.complement() in self.clauses:
+                exists_complement = True
+            else:
+                if clause not in clauses_unique:
+                    clauses_unique.append(clause)
+        if self.operation == Operations.Disjuntion:
+            if exists_complement or exists_T:
+                return Clause(type=ClauseType.T)
+            return Clause(clauses_unique, Operations.Disjuntion)
+        else:
+            if exists_complement or exists_F:
+                return Clause(type=ClauseType.F)
+            return Clause(clauses_unique, Operations.Conjuntion)
+                
+            
             
 class NestedClause:
     def __init__(self, left_clause=Clause(), right_clause=Clause(), operation=Operations.Disjuntion, neg=False):
@@ -94,10 +146,18 @@ class NestedClause:
         self.neg = neg
     
     def __str__(self):
-        if self.neg:        
-            return f'{Operations.Negation.value}(({str(self.left_clause)}) {self.operation.value} ({str(self.right_clause)}))'
+        if isinstance(self.left_clause, Literal):
+            left_str = str(self.left_clause)
         else:
-            return f'({str(self.left_clause)}) {self.operation.value} ({str(self.right_clause)})'
+            left_str = f'({str(self.left_clause)})'
+        
+        if isinstance(self.right_clause, Literal):
+            right_str = str(self.right_clause)
+        else:
+            right_str = f'({str(self.right_clause)})'
+        
+        clause_str = f'{left_str} {self.operation.value} {right_str}'
+        return f'{Operations.Negation.value}({clause_str})' if self.neg else clause_str
         
     def complement(self):
         # Only works if all operations are disjuntions or conjuntions
